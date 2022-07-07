4278232ceb33390db6bd611371f844aa4193deea
peacepursuer <akingbadehammed@aol.com>
2022-07-07 16:23:50 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,26 @@
 #include "main.h"
 /**
- * main - Entry begins
- * Return: nothing, return void
- */
+* main - Entry begins
+* Return: nothing, return void
+*/
 int main(void)
 {
-  putchar(");
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+  _putchar('_');
+  
+  _putchar('p');
+  
+  _putchar('u');
+  
+  _putchar('t');
+  
+  _putchar('c');
+  
+  _putchar('h');
+  
+  _putchar('a');
+  
+  _putchar('r');
+  
+  _putchar('\n');
 return (0);
 }
