859aea8b9df43fb716b18838fa4d98892057cfbb
peacepursuer <akingbadehammed@aol.com>
2022-07-11 16:55:29 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _abs - Computes the absolute value of an imteger
+* @i: The integer to compute with
+*
+* Return: The absolute value of i
+*/
+int _abs(int i)
+{
+return (i < 0 ? I * -1 : i);
+}
