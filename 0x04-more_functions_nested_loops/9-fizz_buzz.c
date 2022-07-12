ec5988a8ed3fa8c34012d664b51b079a2a30570d
peacepursuer <akingbadehammed@aol.com>
2022-07-12 14:33:38 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ for (i = 1; i <= 100; i++)
 if ((i % 3 == 0) && (i % 5 == 0))
 printf("FizzBuzz");
 else if (i % 3 == 0)
-rintf("Fizz");
+printf("Fizz");
 else if (i % 5 == 0)
 printf("Buzz");
 else
