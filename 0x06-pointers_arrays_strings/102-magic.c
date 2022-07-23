61f0f23237db6e91a02a282930092b9b46a2f641
peacepursuer <akingbadehammed@aol.com>
2022-07-22 17:01:41 -0700

    Task 10 done

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-/**
-* main - multiplies two numbers.
-* @argc: number of command line arguments.
-* @argv: array that contains the program command line arguments.
-* Return: 0 - success.
+
+int main(void)
+{
+int n;
+int a[5];
+int *p;
+
+a[2] = 1024;
+p = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
 */
-int main(int argc, char *argv[])
-{
-if (argc != 3)
-{
-printf("Error\n");
-return (1);
-}
-printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
 return (0);
 }
